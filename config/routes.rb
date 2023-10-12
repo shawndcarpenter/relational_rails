c2fcb0a10737ebe2bfd897fec8ae38bc23696144
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-12 12:12:43 -0600

    Rewrite routes to properly reference controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/orchestras', to: 'orchestras#index'
-  #get '/orchestras/:id', to: 'orchestras/show'
+  get '/orchestras/:id', to: 'orchestras#show'
+  get '/musicians', to: 'musicians#index'
+  get '/musicians/:id', to: 'musicians#show'
 end
