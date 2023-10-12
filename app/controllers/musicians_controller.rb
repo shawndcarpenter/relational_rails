d44910f4fb4e6bb312ffffac5ce589ad0bf9c6cf
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-12 12:09:45 -0600

    Write show methods

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -2,4 +2,8 @@ class MusiciansController < ApplicationController
   def index
     @musicians = Musician.all
   end
+
+  def show
+    @musician = Musician.find(params[:id])
+  end
 end
--- app/controllers/orchestras_controller.rb
+++ app/controllers/orchestras_controller.rb
@@ -2,4 +2,8 @@ class OrchestrasController < ApplicationController
   def index
     @orchestras = Orchestra.all
   end
+
+  def show
+    @orchestra = Orchestra.find(params[:id])
+  end
 end
