88a1a79ac1cca07bfd70aab3896b1a1fea559ffc
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-15 17:06:44 -0600

    Complete functionality for alphabetical child sort

--- app/controllers/orchestra_musicians_controller.rb
+++ app/controllers/orchestra_musicians_controller.rb
@@ -1,6 +1,20 @@
 class OrchestraMusiciansController < ApplicationController
   def index
     @orchestra = Orchestra.find(params[:id])
-    @musicians = @orchestra.musicians
+    @musicians = @orchestra.musicians.order(params[:sort])
+  end
+
+  def new
+  end
+
+  def create
+    #binding.pry
+    @musician = @orchestra.musicians.create({
+      name: params[:name],
+      instrument: params[:instrument], 
+      position: params[:position], 
+      years_active: params[:years_active]
+    })
+    redirect_to "/orchestras/#{@orchestra.id}"
   end
 end
